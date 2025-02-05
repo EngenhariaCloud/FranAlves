65ef72d807a2868fd9e0a66b986d15b5e33bd2b4
Engenharia Cloud <hodrigodionath@gmail.com>
2025-02-05 15:18:49 -0300

    ate

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Script simples para validação do formulário
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Sua mensagem foi enviada com sucesso!');
+});
